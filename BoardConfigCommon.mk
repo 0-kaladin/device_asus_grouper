a16320d5d3b18a1d1889ca5f94ae9f9c68ee729c
0-kaladin <0.kaladin@gmail.com>
2013-05-29 10:33:26 -0400

    add vfp and neon

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -35,6 +35,8 @@ TARGET_CPU_SMP := true
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a9
+ARCH_ARM_HAVE_VFP := true
+ARCH_ARM_HAVE_NEON := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_KERNEL_SOURCE = kernel/asus/grouper
